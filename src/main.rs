8c2065e44dac2ee8f560a5b977ce2c589b122a10
314ShadePi <erikkirkegaardstage@gmail.com>
2022-07-29 10:55:48 +0200

    Get user input up and running
    Get asking, tokio and async_std up and running, and ask first question.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use asking::error::Processing;
+use std::time::Duration;
+use tokio::net::TcpListener;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+
+#[tokio::main]
+async fn main() {
+    loop {
+        loop {
+            let eula_accepted = asking::yn()
+                .message("Please agree to the MINECRAFT END USER LICENSE AGREEMENT at https://account.mojang.com/documents/minecraft_eula before continuing. [y/N]")
+                .default_value(false)
+                .ask();
+
+            match async_std::task::block_on(eula_accepted) {
+                Ok(true) => println!("EULA accepted"),
+                Ok(false) => {
+                    println!("You have to agree to the MINECRAFT END USER LICENSE AGREEMENT at https://account.mojang.com/documents/minecraft_eula before continuing.");
+                    continue;
+                }
+                Err(_processing) => {
+                    println!("You have to agree to the MINECRAFT END USER LICENSE AGREEMENT at https://account.mojang.com/documents/minecraft_eula before continuing.");
+                    continue;
+                }
+            }
+        }
+    }
 }
