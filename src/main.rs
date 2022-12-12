af92ee701a4defd33a2f2844e75f01a45e25a9b2
314ShadePi <erikkirkegaardstage@gmail.com>
2022-12-12 14:44:30 +0100

    Logging

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+use std::fs::File;
+
+use simplelog::*;
+
 fn main() {
-    println!("Hello, world!");
+    let log_file = match home::home_dir() {
+        Some(home_dir) => home_dir.join(".emcs.log"),
+        None => {
+            CombinedLogger::init(vec![TermLogger::new(
+                LevelFilter::Trace,
+                Config::default(),
+                TerminalMode::Mixed,
+                ColorChoice::Auto,
+            )])
+            .unwrap();
+
+            log::error!("Failed to get home directory for logging!");
+            std::process::exit(1)
+        }
+    };
+    CombinedLogger::init(vec![
+        TermLogger::new(
+            LevelFilter::Trace,
+            Config::default(),
+            TerminalMode::Mixed,
+            ColorChoice::Auto,
+        ),
+        WriteLogger::new(
+            LevelFilter::Trace,
+            Config::default(),
+            File::create(log_file).unwrap(),
+        ),
+    ])
+    .unwrap();
+
+    log::info!("Starting app.");
+    log::trace!("Hello World!");
+    log::trace!("a trace example");
+    log::debug!("deboogging");
+    log::info!("such information");
+    log::warn!("o_O");
+    log::error!("boom");
 }
