175f8b77c3fc8aef9ff4266a6675620ab3fb8760
314ShadePi <erikkirkegaardstage@gmail.com>
2022-12-12 16:27:11 +0100

    Move logger initialization into seperate block

--- src/main.rs
+++ src/main.rs
@@ -3,35 +3,38 @@ use std::fs::File;
 use simplelog::*;
 
 fn main() {
-    let log_file = match home::home_dir() {
-        Some(home_dir) => home_dir.join(".emcs.log"),
-        None => {
-            CombinedLogger::init(vec![TermLogger::new(
+    // Initialize logger
+    {
+        let log_file = match home::home_dir() {
+            Some(home_dir) => home_dir.join(".emcs.log"),
+            None => {
+                CombinedLogger::init(vec![TermLogger::new(
+                    LevelFilter::Trace,
+                    Config::default(),
+                    TerminalMode::Mixed,
+                    ColorChoice::Auto,
+                )])
+                .unwrap();
+
+                log::error!("Failed to get home directory for logging!");
+                std::process::exit(1)
+            }
+        };
+        CombinedLogger::init(vec![
+            TermLogger::new(
                 LevelFilter::Trace,
                 Config::default(),
                 TerminalMode::Mixed,
                 ColorChoice::Auto,
-            )])
-            .unwrap();
-
-            log::error!("Failed to get home directory for logging!");
-            std::process::exit(1)
-        }
-    };
-    CombinedLogger::init(vec![
-        TermLogger::new(
-            LevelFilter::Trace,
-            Config::default(),
-            TerminalMode::Mixed,
-            ColorChoice::Auto,
-        ),
-        WriteLogger::new(
-            LevelFilter::Trace,
-            Config::default(),
-            File::create(log_file).unwrap(),
-        ),
-    ])
-    .unwrap();
+            ),
+            WriteLogger::new(
+                LevelFilter::Trace,
+                Config::default(),
+                File::create(log_file).unwrap(),
+            ),
+        ])
+        .unwrap();
+    }
 
     log::info!("Starting app.");
     log::trace!("Hello World!");
